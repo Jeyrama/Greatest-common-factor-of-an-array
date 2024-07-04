943b47b28cbd4d9562967ceae219aeb9a670998d
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-07-04 11:14:14 -0700

    Update Greatest-common-factor-of-an-array.js

--- Greatest-common-factor-of-an-array.js
+++ Greatest-common-factor-of-an-array.js
@@ -9,3 +9,11 @@ greatestCommonFactor([46, 14, 20, 88]); // --> 2
 
 
 // Solution
+
+function greatestCommonFactor(array) {
+  function gcf(a, b) {
+    if (b === 0) return a;
+    return gcf(b, a%b);
+  }
+  return array.reduce(gcf);
+}
